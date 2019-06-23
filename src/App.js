f5c54782165b6c1d953579dbe3241b3576997825
AbhilashMessi <35174195+AbhilashMessi@users.noreply.github.com>
2019-06-23 16:38:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React from "react";
+import styles from "./App.module.css";
+import openSocket from "socket.io-client";
+import Chat from "./containers/Chat";
+import Loader from "./components/Loader";
+import Logo from "./components/Logo";
+
+const socket = openSocket(); // Defaults to current url
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      partner: {},
+      handle: "",
+      connection_success: false,
+      initiated_connection: false
+    };
+  }
+
+  componentDidMount() {
+    // Register user
+    socket.on("connection_success", ({ partner }) => {
+      if (partner.id !== -1) {
+        this.setState({
+          partner,
+          connection_success: true
+        });
+      }
+    });
+  }
+
+  // Find partner
+  connect = () => {
+    if (this.state.handle.length === 0) {
+      alert("Oops! You don't seem to have a handle. Please try again");
+      return;
+    }
+
+    this.setState({
+      initiated_connection: true
+    });
+
+    socket.emit("establish_connection", { handle: this.state.handle });
+  };
+
+  render() {
+    if (this.state.connection_success) {
+      return (
+        <div className={styles.App}>
+          <Chat
+            partner={this.state.partner}
+            socket={socket}
+            handle={this.state.handle}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.App}>
+          <Logo />
+          <p
+            style={{
+              textAlign: "center"
+            }}
+          >
+            <strong> Connect with awesome developers around the world! </strong>
+          </p>
+          <div className={styles.input_box}>
+            <input
+              placeholder="Handle"
+              onChange={e =>
+                this.setState({
+                  handle: e.target.value
+                })
+              }
+            />
+            <button
+              onClick={this.connect}
+              disabled={this.state.initiated_connection}
+            >
+              Connect
+            </button>
+          </div>
+          {this.state.initiated_connection && !this.state.connection_success ? (
+            <div>
+              <p className={styles.status}>
+                No one is here.We 'll connect you automatically as soon as
+                someone come...
+              </p>
+              <Loader />
+            </div>
+          ) : null}
+          <p
+            style={{
+              marginTop: "10rem",
+              fontSize: "1.2rem",
+              textAlign: "center"
+            }}
+          >
+            We respect your privacy and don 't store your messages in any
+            manner, so you can talk freely{" "}
+            <span role="img" aria-label="smily">
+              🙃
+            </span>
+          </p>
+        </div>
+      );
+    }
+  }
+}
